32bd87f4df397fc9e2ec5efac52484ba2dd527b8
vinayak-kumar-sharma <vinayaksharma07@yahoo.com>
2024-11-24 21:27:25 +0530

    new commit

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const blogRoutes = require("./routes/articles.js")
 const methodOverride = require('method-override');
+require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended:false }))
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-mongoose.connect('mongodb+srv://vinayaksharma07:Mongo$123@cluster0.4it5p.mongodb.net/')
+mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
